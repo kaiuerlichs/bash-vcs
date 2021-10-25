c87a666b136188513659bbf98430eed2d0a08e14
kaiuerlichs <2421101@dundee.ac.uk>
2021-10-25 16:37:26 +0100

    Added comments

--- scripts/add.sh
+++ scripts/add.sh
@@ -5,6 +5,7 @@ CYAN="\033[0;36m"
 NOCL="\033[0m"
 
 add_function() {
+    # Check if any of these characters are within contained within the specfied repository name
     if [[ $1 =~ ['.;!@#$%^&*()\/<>|:'] ]]
     then
         echo -e "$PREFIX The specified repository name is invalid. Avoid the following characters:"
@@ -12,31 +13,40 @@ add_function() {
         return 0;
     fi
 
-    if [ ! $# -gt 1 ]
+    # Check if a repository was specified
+    if [ $# -eq 0 ]
     then
-        echo -e "$PREFIX No file(s) were specified."
+        echo -e "$PREFIX No repository was specified."
+        return 0;
     fi
-
-    REPO=$HOME/cms/repositories/$1
-
+    # Check whether specified repository exists
     if [ ! -e $REPO ]
     then
         echo -e "$PREFIX The repository \"$1\" does not exist."
         return 0;
     fi
+    REPO=$HOME/cms/repositories/$1
 
+    # Check if any files were specified
+    if [ ! $# -gt 1 ]
+    then
+        echo -e "$PREFIX No file(s) were specified."
+        return 0;
+    fi
     FILES=${@:2}
 
-
-
+    # Attempt to add each file to the repository separately
     for f in $FILES
     do
+        # Check if the file exists
         if [ -e $f ]
         then
+            # Check if the file already exists in the repository
             if [ -e $REPO/$f ]
             then
                 echo -e "$PREFIX The file \"$f\" already exists in the repository. Did you mean ${CYAN}cms checkin${NOCL}?"
             else
+                # Copy and create file table entry
                 cp $f $REPO
                 echo "$f;in;---" >> $REPO/.cms/file_table
                 echo -e "$PREFIX The file \"$f\" has been added to the repository."
