c654f8d41371e2adb04fa7a5d79cce2f72281e21
kaiuerlichs <2421101@dundee.ac.uk>
2021-10-27 22:05:39 +0100

    one more comment he

--- scripts/checkin.sh
+++ scripts/checkin.sh
@@ -57,12 +57,15 @@ checkin_function() {
                 read -p "$(echo -e "$PREFIX Show diff view? [y/N] ")" option
                 case $option in
                     [Yy]* ) 
+                        # Show diff view for original and replacement file
                         echo -e "$PREFIX Displaying diff for file checkin on $FILE"
                         diff $REPO/$FILE $FILE
+
+                        # Confirm user wants to continue
                         read -p "$(echo -e "$PREFIX Do you still wish to check-in? [Y/n] ")" option
                         case $option in
                             [Nn]* ) 
-                                echo -e "$PREFIX Aborting..."
+                                echo -e "$PREFIX Aborting checkin..."
                                 return 0;
                                 ;;
                         esac
