9068a0af5aec49f4aecfa42b47ab0dd5edf75179
nicolejackson2 <2415277@dundee.ac.uk>
2021-10-29 16:05:09 +0100

    started help script

--- scripts/help.sh
+++ scripts/help.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 # Displays a help page explaining all the commands of the cms script
+
+echo "Add: add files to a repository."
+echo "Check in: check in files."
+echo "Check out: check out files."
+echo "Create: create repository folder, .cms folders and file table."
+echo "Forward: forward command to run on the repository."
+echo "Help: help page for commands(current page)."
+echo "Import: import repositiry from archive."
+echo "List: list the repository files."
+echo "Log: show log file for selected file."
+echo "Remove: remove file from repository."
+echo "Reset: reset checkout state of the file."
+echo "Revert-snap: revert to snapshot version of repository."
+echo "Revert: revert file to previous iteration."
+echo "Snapshot: create snapshot archive of repository."
+echo "Versions: display file versions list."
+
