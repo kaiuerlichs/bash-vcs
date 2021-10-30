ee2075e07c19accb9a454059fb9b61085af29972
chrisomay <2418120@dundee.ac.uk>
2021-10-30 16:53:18 +0100

    removeFile complete

--- scripts/remove.sh
+++ scripts/remove.sh
@@ -1,22 +1,55 @@
-# Takes a repo name and file name
-# Deletes a file, its logs and versions from the repository permanently
-# Display applicable warnings and ask user to confirm they want to proceed
-
 #!/bin/bash
+
+PREFIX="\033[0;36m[CMS]\033[0m"
+
 remove_function{
 #recieve user input
+    DATE=$(date "+[%d-%m-%Y | %T]")
+    # Check if a repository was specified
+    if [ $# -eq 0 ]
+    then
+        echo -e "$PREFIX No repository was specified."
+        return 0;
+    fi
+    # Check if any of these characters are within contained within the specfied repository name
+    if [[ $1 =~ ['.;!@#$%^&*()\/<>|:'] ]]
+    then
+        echo -e "$PREFIX The specified repository name is invalid. Avoid the following characters:"
+        echo ". ; ! @ # $ % ^ & * ( ) \ / < > | :"
+        return 0;
+    fi
+    REPO=/cms/repositories/$1
+    # Check whether specified repository exists
+    if [ ! -e $REPO ]
+    then
+        echo -e "$PREFIX The repository \"$1\" does not exist."
+        return 0;
+    fi
 
-read -p "Enter the name of the file you want to remove: " file
-#validate if file exists and recieve user confirmation
-if [-f $file]; then
-   rm -i "$file"
-   #check file has been removed
-   if [-f $file]; then
-      echo "Error, we have encountered a problem and $file has not been removed"
-   else
-      echo "Success $file removed"
-   fi
-else
-   echo "Error, the file $file does not exist"
-fi
+    # Check if a file was specified
+    if [ ! $# -gt 1 ]
+    then
+        echo -e "$PREFIX No file was specified."
+        return 0;
+    fi
+    FILE=$2
+
+    #validate if file exists and recieve user confirmation
+    if [-f $FILE]; then
+        read -p "$(echo -e "$PREFIX Are you sure you want to delete this file permanently? [y/N] ")" option
+        case $option in
+            [Yy]* ) 
+                rm $REPO/$FILE
+                rm $REPO/.cms/logs/$FILE
+                rm -R $REPO/.cms/versions/$FILE
+                line=$(grep $FILE $REPO/.cms/file_table)
+                sed -i "/$line/d" $REPO/.cms/file_table
+                echo -e "$PREFIX File was successfully deleted!"
+                ;;
+            * )
+                echo -e "$PREFIX Aborting file delete..."
+                return 0;
+                ;;
+        esac
+    fi
 }
