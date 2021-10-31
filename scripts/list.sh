47f6db52e4cc244d58c48f1b9a809e2681e55ec9
chrisomay <2418120@dundee.ac.uk>
2021-10-31 15:42:57 +0000

    listing repositories

--- scripts/list.sh
+++ scripts/list.sh
@@ -1,3 +1,21 @@
+#!/bin/bash
 # Takes either no parameters, or a repo name as parameter
 # Without parameter, display all repositories
 # With repo parameter, display all files in repository
+
+PREFIX="\033[0;36m[CMS]\033[0m"
+
+list_function(){
+    # Check if a repository was specified
+    if [ $# -eq 0 ]
+    then
+        ls /cms/repositories | while read line
+        do
+            echo -e "$PREFIX $line\tLast edited: $(date -r  /cms/repositories/$line "+%d-%m-%Y %H:%M:%S")"
+        done
+    else
+    
+    fi
+    
+    
+}
