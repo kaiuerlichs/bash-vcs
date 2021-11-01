b609eb3ed92e5d2f4636a79a80793e981165f190
kaiuerlichs <2421101@dundee.ac.uk>
2021-11-01 09:54:27 +0000

    .

--- scripts/help.sh
+++ scripts/help.sh
@@ -2,40 +2,50 @@
 # Displays a help page explaining all the commands of the cms script
 
 PREFIX="\033[0;36m[CMS]\033[0m"
+CYAN="\033[0;36m"
+NOCL="\033[0m"
+
 
 help_function () {
 
-echo "Add: cms add <repository> <file(s)>
-      add files to a repository."
-echo "Check in: cms checkin <repository> <file>
-      check in files."
-echo "Check out: cms checkout <repository> <file>
-      check out files."
-echo "Create: cms create <repository name>
-      alias init
-      create repository folder, .cms folders and file table."
-echo "Forward: cms forward <repository>
-      forward command to run on the repository."
-echo "Help: cms help <optional command>
-      help page for commands(current page)."
-echo "Import: cms import <archive file> <repository name>
-      import repositiry from archive."
-echo "List: cms list <repository>
-      list the repository files."
-echo "Log: cms log <repository> <file>
-      show log file for selected file."
-echo "Remove: cms remove <repository> <file>
-      remove file from repository."
-echo "Reset: cms reset <repository> <file> <options>
-      -f: forced mode
-      reset checkout state of the file."
-echo "Revert-snap: cms revert-snap <repository> <snapshot>
-      revert to snapshot version of repository."
-echo "Revert: cms revert <repository> <file> <version>
-      revert file to previous iteration."
-echo "Snapshot: cms snapshot create <repository> <optional destination>
-      create snapshot archive of repository."
-echo "Versions: cms versions <repository> <file>
-      display file versions list."
+      echo -e "Add: ${CYAN}cms add <repository> <file(s)>${NOCL}
+            Add file(s) to a repository"
+      echo -e "Check in: ${CYAN}cms checkin <repository> <file>${NOCL}
+            Check in file"
+      echo -e "Check out: ${CYAN}cms checkout <repository> <file>${NOCL}
+            Check out file"
+      echo -e "Create: ${CYAN}cms create <repository name>${NOCL}
+            alias init
+            Create a new repository"
+      echo -e "Forward: ${CYAN}cms forward <repository>${NOCL}
+            Forward command to run on the repository"
+      echo -e "Help: ${CYAN}cms help${NOCL}
+            Help page for commands"
+      echo -e "Import: ${CYAN}cms import <archive file> <repository name>${NOCL}
+            Import repositiry from archive"
+      echo -e "List: ${CYAN}cms list${NOCL}
+            List all repositories"
+      echo -e "List: ${CYAN}cms list <repository>${NOCL}
+            List the repository files"
+      echo -e "Log: ${CYAN}cms log <repository> <file>${NOCL}
+            Show log file for selected file"
+      echo -e "Remove: ${CYAN}cms remove <repository> <file>${NOCL}
+            Remove file from repository"
+      echo -e "Reset: ${CYAN}cms reset <repository> <file> <options>${NOCL}
+            -f: forced mode, resets file state even if the user is not the one currently holding check-out rights
+            Reset checkout state of the file"
+      echo -e "Revert-snap: ${CYAN}cms revert-snap <repository> <snapshot>${NOCL}
+            Revert to a previous snapshot version of repository"
+      echo -e "Revert: ${CYAN}cms revert <repository> <file> <version>${NOCL}
+            Revert file to previous iteration"
+      echo -e "Snapshot: ${CYAN}cms snapshot create <repository> <optional destination>${NOCL}
+            Create a snapshot archive of repository"
+      echo -e "Snapshot: ${CYAN}cms snapshot list <repository> ${NOCL}
+            List all snapshots of a repository"
+      echo -e "Snapshot: ${CYAN}cms snapshot export <repository>${NOCL}
+            alias snapshot copy
+            Export a snapshot of a repository"
+      echo -e "Versions: ${CYAN}cms versions <repository> <file>${NOCL}
+            Display file versions list"
 
 }
